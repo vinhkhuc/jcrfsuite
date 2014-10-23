d9b4e30cbf077f7e3052b9b9114a111fcb486cfc
Justin Harris <justin.harris@maluuba.com>
2014-10-23 11:54:50 -0400

    Pair: getters

--- src/main/java/com/github/jcrfsuite/util/Pair.java
+++ src/main/java/com/github/jcrfsuite/util/Pair.java
@@ -4,4 +4,12 @@ public class Pair<T1, T2> {
     public T1 first;
     public T2 second;
     public Pair(T1 x, T2 y) { first=x; second=y; }
+
+    public T1 getFirst(){
+        return first;
+    }
+
+    public T2 getSecond(){
+        return second;
+    }
 }
