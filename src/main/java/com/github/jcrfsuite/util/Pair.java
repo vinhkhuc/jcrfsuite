7951211357048ac596914ac74f90eb385f44f200
Justin Harris <justin.harris@maluuba.com>
2014-10-23 14:07:31 -0400

    Pair: toString

--- src/main/java/com/github/jcrfsuite/util/Pair.java
+++ src/main/java/com/github/jcrfsuite/util/Pair.java
@@ -1,15 +1,20 @@
 package com.github.jcrfsuite.util;
 
 public class Pair<T1, T2> {
-    public T1 first;
-    public T2 second;
-    public Pair(T1 x, T2 y) { first=x; second=y; }
+	public T1 first;
+	public T2 second;
+	public Pair(T1 x, T2 y) { first=x; second=y; }
 
-    public T1 getFirst(){
-        return first;
-    }
+	public T1 getFirst(){
+		return first;
+	}
 
-    public T2 getSecond(){
-        return second;
-    }
+	public T2 getSecond(){
+		return second;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("{%s, %s}", first, second);
+	}
 }
